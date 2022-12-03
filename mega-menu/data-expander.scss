99b874c0953aa33070f65c0f9d005be5458e3f5a
Piush Bhandari <83722221+piushbhandari@users.noreply.github.com>
2022-12-02 18:27:02 -0600

    Create data-expander.scss

---
+++ mega-menu/data-expander.scss
@@ -0,0 +1,53 @@
+.target-show {
+    animation-duration: .3s;
+    animation-fill-mode: both;
+    animation-timing-function: ease-in-out;
+    animation-name: toggleTargetShow;
+}
+
+.target-hide {
+    animation-duration: .3s;
+    animation-fill-mode: both;
+    animation-timing-function: ease-in-out;
+    animation-name: toggleTargetHide;
+}
+
+[data-expander-target] {
+    display: none;
+}
+
+[data-expander-target].active {
+    display: block;
+}
+
+[data-expander-target].target-show {
+    animation-name: toggleTargetShow;
+}
+
+[data-expander-target].target-hide {
+    animation-name: toggleTargetHide;
+}
+
+@keyframes toggleTargetShow {
+    from {
+        opacity: 0;
+        transform: translateY(-10%);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0%);
+    }
+}
+
+@keyframes toggleTargetHide {
+    from {
+        opacity: 1;
+        transform: translateY(0%);
+    }
+
+    to {
+        opacity: 0;
+        transform: translateY(-10%);
+    }
+}
